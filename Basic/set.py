5e186343152369b26e260473a439de34c14650a0
sanjana pawar <psanjana077@gmail.com>
2023-11-11 00:34:07 +0530

    commit

---
+++ Basic/set.py
@@ -0,0 +1,3 @@
+# A set is a collection which is unordered, unchangeable*, and unindexed.
+# Set items are unordered, unchangeable, and do not allow duplicate values.
+# True and 1 is considered the same value
